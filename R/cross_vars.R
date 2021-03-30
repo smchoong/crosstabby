2150e4650169d264087c053eded8b7f8f8647ea5
Sarah-Marie Choong <choong.sarah.marie@gmail.com>
2021-03-30 13:51:56 -0400

    fix cross_vars logic to work inside mutuate()

--- R/cross_vars.R
+++ R/cross_vars.R
@@ -10,14 +10,12 @@
 #' @examples
 cross_vars <- function(df, var1, var2) {
 
-  var_pat1 <- paste("\\b", var1, "\\b", sep = "")
-  var_pat2 <- paste("\\b", var2, "\\b", sep = "")
+  new_name <- paste(var1, var2, sep = " x ")
+  new_df <- df %>% unite(new_var, c(all_of(var1), all_of(var2)), sep = " x ")
+  new_var <- new_df %>% select(new_var)
+  names(new_var)[1] <- new_name
+  out <- cbind(df, new_var)
 
-  var1_nm <- df %>% select(grep(var_pat1, names(df))) %>% names()
-  var2_nm <- df %>% select(grep(var_pat2, names(df))) %>% names()
-  # df$combined <- paste(df$var1, df$var2, sep = " | ")
-  # new_name <- paste(var1_nm, var2_nm, sep = " | ")
-  # names(df)[names(df) == "combined"] <- new_name
+  return(out)
 
-  return(c(var1_nm, var2_nm))
 }
