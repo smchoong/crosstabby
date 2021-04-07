9ec9849572c2cc838bbfb0eb6d21a80363584fb6
Sarah-Marie Choong <choong.sarah.marie@gmail.com>
2021-04-06 22:21:53 -0400

    add tests

--- tests/testthat/test-big_tabby.R
+++ tests/testthat/test-big_tabby.R
@@ -9,3 +9,24 @@ test_that("when length(row.var)==1 output is a data.frame", {
   )
   expect_s3_class(big_tabby(df,"x"), "data.frame")
 })
+
+test_that("when length(row.var)>1 output is a data.frame", {
+  df <- data.frame(
+    x = c(1, 1, 1, 1),
+    y = c(1, 1, 2, 2),
+    z = c(1, 2, 1, 2)
+  )
+  var<-c("x","y")
+  expect_s3_class(big_tabby(df,var), "data.frame")
+})
+
+test_that("when length(row.var)==1 and length(col.var)==1 output is a data.frame", {
+  df <- data.frame(
+    x = c(1, 1, 1, 1),
+    y = c(1, 1, 2, 2),
+    z = c(1, 2, 1, 2)
+  )
+  var<-c("x","y")
+  by<-"z"
+  expect_s3_class(big_tabby(df,var,by), "data.frame")
+})
